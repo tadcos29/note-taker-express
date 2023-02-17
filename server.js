3a6f4910cf5c319e3d75d8edb2713c2c106f3362
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-02-16 21:30:55 -0500

    Test placeholder GET and POST routes.

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const { appendFile } = require('fs');
 const path = require('path'); //for path-joining needs
-const api = require('./routes/dispatch.js'); //this is for relegating to the dispatch file
+// const api = require('./routes/dispatch.js'); //this is for relegating to the dispatch file
 
-const PORT = 3001; // for easy changing
+const PORT = 3001; // for easy changing // change to the heroku thing
 
 const app = express(); //initialise the app
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true  })); // middleware data-handling utils from express
 
-app.use('/api', api); // redirect any /api/ calls to the dispatch
+// app.use('/api', api); // redirect any /api/ calls to the dispatch
 app.use(express.static('public')); //allow access to public folder?
 
-app.get('/', (req,res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-)
+
+
+app.get('/api/notes', (req,res) => {
+      res.json('Notes GET received.');
+});
+
+app.post('/api/notes', (req,res) => {
+  res.json('Notes POST received.');
+});
+
+app.get('/notes', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.get('*', (req,res) =>
-    res.sendFile(path.join(__dirname, '/public/404.html'))
-)
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
